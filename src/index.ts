75fd548906b5337168e7479d6de43516bd56c053
yanxxmh <xx.yan@tingo66.com>
2019-06-21 11:21:09 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,19 @@ export * from './resource';
 export * from './sdkregister';
 export * from './sensitiveword';
 
+import Admin from './admin';
+import Chatroom from './chatroom';
 import Message from './message';
-export default Message;
+import Group from './group';
+import Resource from './resource';
+import Sdkregister from './sdkregister';
+import Sensitiveword from './sensitiveword';
+export {
+  Admin,
+  Chatroom,
+  Message,
+  Group,
+  Resource,
+  Sdkregister,
+  Sensitiveword,
+};

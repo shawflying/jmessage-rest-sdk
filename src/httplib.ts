8526775f5c5b785cd18b2cd6c10ac3864dfbd3de
yanxxit <yanxxit@163.com>
2019-06-26 20:56:49 +0800

    update

--- src/httplib.ts
+++ src/httplib.ts
@@ -4,55 +4,54 @@
  * @description 将request-promise进行简单封装，使得使用时更加方便
  */
 const rp = require('request-promise');
-const request = require('request');
 const moment = require('moment');
 const querystring = require('querystring');
-const conf = require("./config")
+const conf = require('./config');
 
 /** 获取 headers.Authorization */
-var getAuthorization = () => {
-  const v = conf.AppKey + ":" + conf.MasterSecret;
-  var bff = Buffer.from(v, "utf8")
-  var Authorization = "Basic " + bff.toString("base64");
-  return Authorization
-}
+const getAuthorization = () => {
+  const v = conf.AppKey + ':' + conf.MasterSecret;
+  let bff = Buffer.from(v, 'utf8');
+  let Authorization: string = 'Basic ' + bff.toString('base64');
+  return Authorization;
+};
 
 const current_headers: any = {
   Authorization: getAuthorization()
-}
+};
 
 
 /**
  * 设置通用headers 信息
- * @param {Object} headers 
+ * @param {Object} headers
  */
-const setHeaders = (headers = {}) => {
+const setHeaders = (headers: any = {}) => {
   headers['request-time'] = new Date().getTime();
-  Object.assign(headers, current_headers)
-}
+  Object.assign(headers, current_headers);
+};
 /** 格式化接口信息 */
 const formatRequestInfo = (type: string, uri: string, body: any, headers: any, data: any) => {
   let restime = moment().diff(moment(headers['request-time']), 'milliseconds') + 'ms';
-  let info = [moment().format("YYYY-MM-DD HH:mm:ss"), type, uri, restime]
+  let info = [moment().format('YYYY-MM-DD HH:mm:ss'), type, uri, restime];
 
-  console.log("httplib:--> ", info.join(" "));
-  if (body) console.log("httplib:--> ", "body:" + JSON.stringify(body))
-}
+  console.log('httplib:--> ', info.join(' '));
+  if (body) console.log('httplib:--> ', 'body:' + JSON.stringify(body));
+};
 const SuccessRequest = (type: string, uri: string, body: any, headers: any, data: any) => {
-  formatRequestInfo(type, uri, body, headers, data)
+  formatRequestInfo(type, uri, body, headers, data);
   if (data && data.error && data.error) return data.error;
-  return data
-}
+  return data;
+};
 const ErrorResponse = (type: string, uri: string, body: any, headers: any, err: any) => {
-  formatRequestInfo(type, uri, body, headers, err)
+  formatRequestInfo(type, uri, body, headers, err);
   if (err.error && Array.isArray(err.error) && err.error.length > 0) {
     return err.error[0].error;
   }
   if (err.error && !Array.isArray(err.error) && err.error.error) {
     return err.error.error;
   }
-  return { error: -1, message: "other error", data: err.message }
-}
+  return { error: -1, message: 'other error', data: err.message };
+};
 
 /**
  * POST JSON 请求
@@ -60,39 +59,39 @@ const ErrorResponse = (type: string, uri: string, body: any, headers: any, err: 
  * @param {Object} body 请求内容 json模式
  * @param {Object} headers 请求headers
  */
-export var post = async (uri: string, body: any = {}, headers: any = {}) => {
+export let post = async (uri: string, body: any = {}, headers: any = {}) => {
   try {
-    setHeaders(headers)
-    let data = await rp.post(uri, { body, json: true, headers })
-    return SuccessRequest('POST', uri, body, headers, data)
+    setHeaders(headers);
+    let data = await rp.post(uri, { body, json: true, headers });
+    return SuccessRequest('POST', uri, body, headers, data);
   } catch (err) {
-    return ErrorResponse('POST', uri, body, headers, err)
+    return ErrorResponse('POST', uri, body, headers, err);
   }
-}
+};
 /**
  * GET JSON 请求
  * @param {string} uri 接口地址
  * @param {Object} query 请求内容 json模式
  * @param {Object} headers 请求headers
  */
-export var get = async (uri: string, query: any = {}, headers: any = {}) => {
+export let get = async (uri: string, query: any = {}, headers: any = {}) => {
   try {
-    setHeaders(headers)
-    let [url1, url2] = uri.split('?')
+    setHeaders(headers);
+    let [url1, url2] = uri.split('?');
     let urlPrams = querystring.parse(url2);
     for (let k in urlPrams) {
-      query[k] = urlPrams[k]
+      query[k] = urlPrams[k];
     }
 
     let params = querystring.stringify(query);
-    uri = url1 + (params ? '?' + params : '')
+    uri = url1 + (params ? '?' + params : '');
 
-    let data = await rp.get(uri, { json: true, headers })
-    return SuccessRequest('GET', uri, query, headers, data)
+    let data = await rp.get(uri, { json: true, headers });
+    return SuccessRequest('GET', uri, query, headers, data);
   } catch (err) {
-    return ErrorResponse('GET', uri, query, headers, err)
+    return ErrorResponse('GET', uri, query, headers, err);
   }
-}
+};
 
 /**
  * PUT JSON 请求
@@ -100,13 +99,13 @@ export var get = async (uri: string, query: any = {}, headers: any = {}) => {
  * @param {Object} body 请求内容 json模式
  * @param {Object} headers 请求headers
  */
-export var put = async (uri: string, body: any = {}, headers: any = {}) => {
+export let put = async (uri: string, body: any = {}, headers: any = {}) => {
   try {
-    setHeaders(headers)
+    setHeaders(headers);
     let data = await rp.put(uri, { body, headers, json: true });
-    return SuccessRequest('PUT', uri, body, headers, data)
+    return SuccessRequest('PUT', uri, body, headers, data);
   } catch (err) {
-    return ErrorResponse('PUT', uri, body, headers, err)
+    return ErrorResponse('PUT', uri, body, headers, err);
   }
 };
 /**
@@ -115,12 +114,12 @@ export var put = async (uri: string, body: any = {}, headers: any = {}) => {
  * @param {Object} body 请求内容 json模式
  * @param {Object} headers 请求headers
  */
-export var del = async (uri: string, body: any = {}, headers: any = {}) => {
+export let del = async (uri: string, body: any = {}, headers: any = {}) => {
   try {
-    setHeaders(headers)
+    setHeaders(headers);
     let data = await rp.del(uri, { body, headers, json: true });
-    return SuccessRequest('DEL', uri, body, headers, data)
+    return SuccessRequest('DEL', uri, body, headers, data);
   } catch (err) {
-    return ErrorResponse('DEL', uri, body, headers, err)
+    return ErrorResponse('DEL', uri, body, headers, err);
   }
 };
